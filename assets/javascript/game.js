0c20ea66fb4cb1c5ec1dd785f919cf48c7e5194c
Josh Cosson <joshlcosson@gmail.com>
2019-09-16 00:14:24 -0700

    work on game.js, added a random shuffle for the game generated number and for the crystals. Added if /else logic for determining if the player has won by matching the number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,89 @@
-var redCrystal;
-var blueCrystal;
-var yellowCrystal;
-var greenCrystal;
-var randomNumber; 
-var totalScore; 
-var wins; 
-var loses; 
-
+var wins = 0;
+var losses = 0;
+var randomNumber = 0; 
+var totalScore =0;
+var counter =0;
+var crystalValue =0; 
 //JavaScript function that wraps everything
 $(document).ready(function() {
 
-//$("#random-button").on("click", function() {
-
-  
+       
         var minNumber = 19;
         var maxNumber = 120;
         
         //specify that the random number will be between 19 and 120
-        var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+        var randomNumber = rangeGeneratedNumber(minNumber, maxNumber);
         
         // function to radomly select a number from that range
-        function randomNumberFromRange(min,max)
+        function rangeGeneratedNumber(min,max)
         {
             return Math.floor(Math.random()*(max-min+1)+min);
         }//console log the number for testing
-          console.log(randomNumber);
+      
 
         // ...  dump the random number into our random number paragraph.
         $("p.random-number").text(randomNumber); 
 
+            
+           var crystalValue = $("#btn");
+           // crystalNumber.each(function() {
 
-      });
+           for(var i = 0; i <4; i++) {
+              var random = Math.floor(Math.random() * 11) + 1;
+              $(this).attr({
+                "data-random": random 
+              }); 
+            }
+            $("#btn").on('click', function() { 
+    
+                crystalValue = ($(this).attr("data-random"));
+                crystalValue = parseInt(crystalValue); 
 
 
+            $("#scoreDiv").text(counter); 
+                
+                 counter += crystalValue; 
+                if(counter > randomNumber) {
+                    alert("Sorry, you lost!"); 
+                    losses++;
+                
+                }
+                else if (counter === randomNumber) {
+                    alert("You won!"); 
+                    wins++;
+                    $("winLossP").html(wins); 
+                }
+        
+            });
 
 
+        });
+
+    
+    //             checkIfWon(); 
 
 
+    //     });
+
+    //     function checkIfWon(); {
+
+    //         if (playerTotal === randomNumber) {
+    //             alert("You won!"); 
+    //             reset();
+    //             wins++;
+    //             $("#wins").text(wins);
+    //         }
+        
+    //         else if (userTotal > random) {
+    //             alert("Sorry, you lost!"); 
+    //             reset();
+    //             losses++;
+    //         } 
 
 
+    //     }
+
+    // });
 
 
-
-
-
-        // // Theme Button
-        // $(".redCrystal").on("click", function() {
-        //   audioElement.play();
-        // });
-        // $(".blueCrystal").on("click", function() {
-        //   audioElement.pause();
-        // });
-
-        // // Size Buttons
-        // $(".yellowCrystal").on("click", function() {
-        //   captainPlanet.animate({ height: "300px" });
-        // });
-        // $(".greenCrystal").on("click", function() {
-        //   captainPlanet.animate({ height: "500px" });
-        // });
-      
-
-        // // Keyboard move controls
-        // $(document).keyup(function(e) {
-        //   console.log(e.which);
-        //   switch (e.which) {
-          
-        //   // Move Buttons (Keyboard Down)
-        //   case 40:
-        //     captainPlanet.animate({ top: "+=200px" }, "normal");
-        //     break;
-
-        //     // Move Buttons (Keyboard Right)
-        //   case 39:
-        //     captainPlanet.animate({ left: "+=200px" }, "normal");
-        //     break;
-
-        //     // Move Buttons (Keyboard Up)
-        //   case 38:
-        //     captainPlanet.animate({ top: "-=200px" }, "normal");
-        //     break;
-
-        //     // Move Buttons (Keyboard Left)
-        //   case 37:
-        //     captainPlanet.animate({ left: "-=200px" }, "normal");
-        //     break;
-
-        //   default:
-        //     break;
-      //     }
-     //    });
-    //  });
-
+  //  $("#score").text(userTotal);
