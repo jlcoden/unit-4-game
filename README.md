199a5985cbfa486d23812540320d9e023fc35af2
Josh Cosson <joshlcosson@gmail.com>
2019-10-22 00:45:57 -0700

    Updated README.md file, and updated javascript file

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # unit-4-game
-**Crystal Collector** is a number matching game in which you must click on four crystals and their values to match with the game randmonly generated number. 
+
+**Crystal Collector** is a number matching game in which you must click on four crystals and their values to match with the game randmonly generated number.
 
 Click on each crystal will add a specific amount of points to your total score.
 
 You will win the game if your total score matches to that of the randomly generated number.
 
-You will lose the game if your total score goes above the random number. 
+You will lose the game if your total score goes above the random number.
 
-The value of each crystal is hidden from the player the player clicks on it. 
+The value of each crystal is hidden from the player the player clicks on it.
 
-Each time the game starts, the game will change the values of each crystal. 
+Each time the game starts, the game will change the values of each crystal.
 
-Refreshing the page will clear the game completely, and allow the player to start fresh. 
+Refreshing the page will clear the game completely, and allow the player to start fresh.
 
 **Game Properties**
-* 4 crystals, each with randomly generated numbers (1 - 12)
-* 1 random game generated number that player must match radomly generated between (19 -120)
-* Total Score 
-* Wins Counter
-* Losses Counter
+
+- 4 crystals, each with randomly generated numbers (1 - 12)
+- 1 random game generated number that player must match radomly generated between (19 -120)
+- Total Score
+- Wins Counter
+- Losses Counter
+
+View project at: https://jlcoden.github.io/unit-4-game/
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,88 +1,82 @@
-//Declare global variables 
+//Declare global variables
 
 //wins counter
 var wins = 0;
 //losses counter
 var losses = 0;
 //randomNumber variable for # player must match
-var randomNumber = 0; 
+var randomNumber = 0;
 //variable for total score
-var totalValue; 
+var totalValue;
 //variable for crystal value
 var crystalValue;
 
-
 //JavaScript function begins program when document is loaded
 $(document).ready(function() {
+  //start/reset function
+  function startReset() {
+    //set total value back to 0
+    totalValue = 0;
+    //set crystal value back to 0
+    crystalValue = 0;
 
-        //start/reset function
-       function startReset() {
-            //set total value back to 0
-            totalValue =0;
-            //set crystal value back to 0
-            crystalValue =0;
+    //Random number generater for number player must match
+    randomNumber = Math.floor(Math.random() * 101 + 19);
 
+    //for loop to loop through and create 4 numbers for each crystal
+    for (var i = 0; i < 4; i++) {
+      //set those 4 values to each button
+      var btnname = "#btn" + i;
+      //generating random numbers
+      var random = Math.floor(Math.random() * 11 + 1);
+      //give each button the 'data-random' attribute, which sets the random value
+      $(btnname).attr("data-random", random);
+      console.log(btnname);
+    }
 
-            //Random number generater for number player must match 
-            randomNumber = Math.floor(Math.random()* 101 + 19);
+    //place the random number into our random number paragraph.
+    $("p.random-number").text(randomNumber);
+  }
 
-            //for loop to loop through and create 4 numbers for each crystal
-            for(var i = 0; i <4; i++) {
-                //set those 4 values to each button
-                var btnname="#btn" +i;
-                //generating random numbers 
-                var random = Math.floor(Math.random() * 11 + 1);
-                //give each button the 'data-random' attribute, which sets the random value
-                $(btnname).attr("data-random", random);
-                console.log(btnname);
-            }
+  startReset();
 
-            //place the random number into our random number paragraph.
-            $("p.random-number").text(randomNumber);
-        }
+  //on click button function
+  $(".btn").on("click", function() {
+    //placeholder for Crystal, which will be given the data-random attribute
+    crystalValue = $(this).attr("data-random");
+    //ParseInt the value so that it is changed to an integer and can be added
+    crystalValue = parseInt(crystalValue);
+    console.log(crystalValue);
+    //for each crystalValue add it to the existing totalValue
+    totalValue += crystalValue;
+    //call checkIfWon function
+    checkIfWon();
+  });
 
-   startReset(); 
-
-            //on click button function
-            $(".btn").on('click', function() {
-
-                //placeholder for Crystal, which will be given the data-random attribute
-                crystalValue = ($(this).attr("data-random"));
-                //ParseInt the value so that it is changed to an integer and can be added
-                crystalValue = parseInt(crystalValue);
-                console.log(crystalValue);
-                //for each crystalValue add it to the existing totalValue
-                 totalValue += crystalValue;
-                 //call checkIfWon function
-                checkIfWon(); 
-
-            }); 
-         
-        //If totalValue becomes greater then game generated number, alert player that they loss
-        function checkIfWon() {
-            if(totalValue > randomNumber) {
-                alert("Sorry, you lost!");
-                //increment losses
-                losses++;
-                //create var to include text, plus updated losses number
-                var showLosses= "Losses: " + losses;
-                //show on html page
-                 $('#lossesAmount').text(showLosses);
-                 startReset(); 
-            }
-            //if totalValue equals randomNumber value, alert that the player won. 
-            else if (totalValue === randomNumber) {
-                alert("You won!");
-                //increment wins
-                wins++;
-                 //create var to include text, plus updated wins number
-                var showWins= "Wins: " + wins;
-                   //show on html page
-                $("#winsAmount").text(showWins);
-                //call start reset function
-                startReset(); 
-            }
-            $("#scoreDiv").text(totalValue);
-
-        }
-    }); 
+  //If totalValue becomes greater then game generated number, alert player that they loss
+  function checkIfWon() {
+    if (totalValue > randomNumber) {
+      alert("Sorry, you lost!");
+      //increment losses
+      losses++;
+      //create var to include text, plus updated losses number
+      var showLosses = "Losses: " + losses;
+      //show on html page
+      $("#lossesAmount").text(showLosses);
+      startReset();
+    }
+    //if totalValue equals randomNumber value, alert that the player won.
+    else if (totalValue === randomNumber) {
+      alert("You won!");
+      //increment wins
+      wins++;
+      //create var to include text, plus updated wins number
+      var showWins = "Wins: " + wins;
+      //show on html page
+      $("#winsAmount").text(showWins);
+      //call start reset function
+      startReset();
+    }
+    $("#scoreDiv").text(totalValue);
+  }
+});
